16c2da3ff2ac43f2f3406bd1fecb765e29b63cdf
Michael House <michaelhouse@gmx.us>
2017-10-02 06:34:18 -0500

    working on toggle buttons

--- inc/application.hpp
+++ inc/application.hpp
@@ -14,15 +14,21 @@ namespace mcalc {
 			Glib::RefPtr<Gtk::Builder>		builder;
 			json datastore;
 
-			Gtk::SpinButton*	fs_sfpm_input;
-			Gtk::SpinButton*	fs_drillsize_input;
-			Gtk::ComboBoxText*	fs_materials_input;
-			Gtk::Button*		fs_materials_btn;
-			Gtk::Label*			fs_materials_output;
+			Gtk::Grid*			fs_materials_grid;
+				Gtk::RadioButton*	fs_mill_select;
+				Gtk::RadioButton*	fs_lathe_select;
+				Gtk::SpinButton*	fs_sfpm_input;
+				Gtk::SpinButton*	fs_drillsize_input;
+				Gtk::ComboBoxText*	fs_materials_input;
+				Gtk::Button*		fs_materials_btn;
+				Gtk::Label*			fs_materials_output;
+
+			Gtk::Grid*			fs_lathe_grid;
 
 		protected:
 			void on_material_sfpm_button_clicked();
 			void on_material_input_changed();
+			void on_fs_select_toggled();
 
 		public:
 			Application(Glib::RefPtr<Gtk::Builder> b);
--- src/application.cpp
+++ src/application.cpp
@@ -11,18 +11,33 @@ namespace mcalc {
 		this->builder = b;
 		this->datastore = get_data();
 
-		b->get_widget("fs_sfpm_input",this->fs_sfpm_input);
-		b->get_widget("fs_drillsize_input",this->fs_drillsize_input);
-		b->get_widget("fs_materials_btn",this->fs_materials_btn);
-		b->get_widget("fs_materials_input",this->fs_materials_input);
-		b->get_widget("fs_materials_output",this->fs_materials_output);
+		b->get_widget("fs_materials_grid",this->fs_materials_grid);
+			b->get_widget("fs_mill_select",this->fs_mill_select);
+			b->get_widget("fs_lathe_select",this->fs_lathe_select);
+			b->get_widget("fs_sfpm_input",this->fs_sfpm_input);
+			b->get_widget("fs_drillsize_input",this->fs_drillsize_input);
+			b->get_widget("fs_materials_btn",this->fs_materials_btn);
+			b->get_widget("fs_materials_input",this->fs_materials_input);
+			b->get_widget("fs_materials_output",this->fs_materials_output);
+
+		b->get_widget("fs_lathe_grid",this->fs_lathe_grid);
+
 
 		/* -------------------------------------------------------------
 		*	Initialize widgets that need data */
+
+		// load fs_materials_data into combobox "fs_materials_input"
 		json data = this->datastore["fs_materials_data"];
 		for (json::iterator it = data.begin(); it != data.end(); ++it) {
 			this->fs_materials_input->append(it.key());
 		}
+
+		// set the active value in fs_materials_input
+		this->fs_materials_input->set_active(0);
+		this->on_material_input_changed();
+
+		// hide the lathe grid
+		this->fs_lathe_grid->hide();
 
 
 		/* -------------------------------------------------------------
@@ -31,6 +46,8 @@ namespace mcalc {
 			&Application::on_material_input_changed));
 		this->fs_materials_btn->signal_clicked().connect(sigc::mem_fun(*this,
 			&Application::on_material_sfpm_button_clicked));
+		this->fs_mill_select->signal_toggled().connect(sigc::mem_fun(*this,
+			&Application::on_fs_select_toggled));
 
 	}
 
@@ -47,6 +64,14 @@ namespace mcalc {
 		double rpm = (4.0*s)/d;
 		if(!std::isinf(rpm)){
 			this->fs_materials_output->set_text(std::to_string(rpm));
+		}
+	}
+
+	void Application::on_fs_select_toggled(){
+		if(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(this->fs_mill_select))){
+			std::cout << "MILL" << std::endl;
+		} else {
+			std::cout << "LATHE" << std::endl;
 		}
 	}
 
