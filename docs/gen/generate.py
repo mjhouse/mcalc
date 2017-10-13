a565120a5e17469305159071a903ac4ff7976d46
Michael House <michaelhouse@gmx.us>
2017-10-13 04:59:23 -0500

    working on desktop

--- docs/gen/generate.py
+++ docs/gen/generate.py
@@ -1,95 +1,97 @@
 import re
 import csv
 import sys
+from collections import defaultdict
 
-TITLES = 0
-DATA_START = 1
-DATA_END = 10
+from pprint import PrettyPrinter
 
-COL_LENGTH = 10
-DATA_RANGE = range(1,10)
-TOOL_RANGE = range(2,14)
+pp = PrettyPrinter(indent=2)
 
-def gen_materials_json( csv ):
-	code = "json material = {\n"
-	data = {}
-	for row in csv[1:-1]:
-		try:
-			hards = [v.strip() for v in re.split('(\n|\t|\s+)',row[1]) if v.strip()]
-			string = row[0].replace('\n',' ')
-			if string.strip():
-				s_list = string.split(':')
-				title = s_list[0].strip()
-				desigs = [d.strip() for d in s_list[1].split(',') if d.strip()]
-				if title and desigs:
-					if title not in data:
-						data[title] = {}
-					for d in desigs:
-						if d not in data[title]: data[title][d] = []
-						for h in hards:
-							if h not in data[title][d]:
-								data[title][d].append(h)
+def get_jtitle( row ):
+	try:
+		title = row[0].split(':')[0].strip()
+	except: pass
+	return title
 
-		except:
-			pass
+def get_jdesignations( row ):
+	desigs = {}
+	try:
+		desigs = {d.strip():{} for d in row[0].split(':')[1].split(',') if d.strip()}
+	except: pass
+	return desigs
+
+def get_jtoolkind( header ):
+	header = header.split(':')
+	tool = header[0].strip()
+	kind = re.search('\((.*)\)', header[1])
+	if kind: kind = kind.group(1)
+	else: kind = ''
+
+	return tool, kind.lower()
+
+def get_jfeedspeed( cell ):
+	cell = [ float(c.strip()) for c in cell.split() if c.strip() ]
+	return cell
+
+def get_jhardness( row ):
+	cell = row[1]
+	cell = {c.strip():{} for c in cell.split() if c.strip()}
+	return cell
+
+def generate_json( data ):
+	rstart,rend = 0, 11
+	cstart,cend = 4, 15
+
+	json = {}
+	for i,row in enumerate(data[rstart:rend]):
+		title			= get_jtitle(row)
+		designations	= get_jdesignations(row)
+		hardnesses		= get_jhardness(row)
+
+		if title and designations:
+			tdata = json.setdefault(title,{})
+			for desig in designations:
+				ddata = tdata.setdefault(desig,{})
+				for hard in hardnesses:
+					hdata = ddata.setdefault(hard,{})
+
+					for j,col in enumerate(row[cstart:cend]):
+						header = data[0][j+cstart]
+						tool, kind  = get_jtoolkind( header )
+						feed, speed = get_jfeedspeed( col )
+
+						odata = hdata.setdefault(tool,{})
+						kdata = odata.setdefault(kind,{})
+						fdata = kdata.setdefault('feed',[])
+						sdata = kdata.setdefault('speed',[])
+
+						fdata.append(feed)
+						sdata.append(speed)
 
 
-	for j,item in enumerate(data.items()):
-		title,desigs = item
-		code += '\t{{"{}",{{\n'.format(title)
-		for i,d in enumerate(desigs):
-			code += '\t\t{{"{}",{{{{ "hardness", {{"{}"}}}}}}}}'.format(d,'","'.join(desigs[d]))
-			if i == len(desigs)-1: code += '\n'
-			else: code += ',\n'
+					#if tool not in v: v[tool] = {}
+					#if kind not in v[tool]: v[tool][kind] = {}
+					#for hard in hardnesses:
+					#	if hard not in v[tool][kind]:
+					#		v[tool][kind][hard] = {}
 
-		if j == len(data.items())-1: code += '\n\t}}\n'
-		else: code += '\n\t}},\n'
-	code += '};'
+						#if 'feeds' not in v[tool][kind][hard]:
+						#	v[tool][kind][hard]['feeds'] = []
+						#if 'speeds' not in v[tool][kind][hard]:
+						#	v[tool][kind][hard]['speeds'] = []
 
-	return code
+						#v[tool][kind][hard]['feeds'].append(feed)
+						#v[tool][kind][hard]['speeds'].append(speed)
 
-def get_column(idx,csv):
-	col = []
-	for row in csv[:-1]:
-		col.append(row[idx])
-	return col
-
-def get_designations(idx,csv):
-	global DATA_RANGE
-
-	cell = csv[idx][0]
-	data = []
-	if idx in DATA_RANGE:
-		data = [d.strip() for d in cell.split(':')[1].split(',') if d.strip()]
-	return data
-
-
-def gen_tool_json(csv):
-	global DATA_START
-	global DATA_END
-	global TOOL_RANGE
-	data = {}
-	for i in TOOL_RANGE:
-		col = get_column(i,csv)
-
-		header = col[0].split(':')
-		name = header[0].strip()
-		_type = re.search('\((.*)\)', header[1])
-		if not _type: continue
-
-		_type = _type.group(1)
-		data[name].update({_type:{}})
-		#for j,contents in enumerate(col[1:10]):
-		#	d = get_designations(j,csv)
-
-	print(data)
+	return json
 
 
 def generate_cpp( fn ):
 	with open(fn,newline='') as f:
 		data = list(list(r) for r in csv.reader(f))
-		material_code	= gen_materials_json(data)
-		tool_code		= gen_tool_json(data)
+		code = generate_json(data)
+
+		pp.pprint(code)
 
 
 
